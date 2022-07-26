6c75ea56e90db55c79f540a529de229013bf6207
dzgoldman <dzgoldman@wesleyan.edu>
2022-07-26 12:18:54 -0400

    update pin, logo, config stuff

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,123 +1,118 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'ignore',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "Arb",
+  tagline: "L2s are cool",
+  url: "https://nitro-docs-beta.netlify.app/",
+  baseUrl: "/",
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "OffchainLabs", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"]
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: '../nitro/docs/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: "../nitro/docs/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          routeBasePath: '/',
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: "/",
+          editUrl: "https://github.com/OffchainLabs/nitro/tree/master/docs"
         }
-      }),
-    ],
+      })
+    ]
   ],
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'arbitrum-sdk',
-        path: '../arbitrum-sdk',
-        routeBasePath: 'sdk',
-        sidebarPath: require.resolve('./sidebars.js'),
+        id: "arbitrum-sdk",
+        path: "../arbitrum-sdk",
+        routeBasePath: "sdk",
+        sidebarPath: require.resolve("./sidebars.js")
         // ... other options
-      },
-    ],
+      }
+    ]
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: "Arbitrum Dev Center",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg"
         },
         items: [
           {
-            type: 'doc',
-            docId: 'arbos/ArbOS',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "arbos/ArbOS",
+            position: "left",
+            label: "Docs"
           },
-          {to: '/sdk', label: 'Sdk', position: 'left'},
+          { to: "/sdk", label: "Sdk", position: "left" },
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+            href: "https://github.com/OffchainLabs/nitro",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/sdk',
-              },
-            ],
+                label: "Tutorial",
+                to: "https://github.com/OffchainLabs/arbitrum-tutorials"
+              }
+            ]
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Discord",
+                href: "https://discord.gg/ZpZuw7p"
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
+                label: "Twitter",
+                href: "https://twitter.com/ArbitrumDevs"
+              }
+            ]
+          }
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Offchain Labs, Built with Docusaurus.`
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        darkTheme: darkCodeTheme
+      }
+    })
 };
 
 module.exports = config;
