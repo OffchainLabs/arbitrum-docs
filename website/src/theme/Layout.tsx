0c7742f0498f168a7717a1e2f6b1cfe268947b9d
dzgoldman <dzgoldman@wesleyan.edu>
2022-09-06 14:12:20 -0400

    generate meta tag preview from doc id

--- website/src/theme/Layout.tsx
+++ website/src/theme/Layout.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import OriginalLayout from "@theme-original/Layout";
 import Head from "@docusaurus/Head";
+import { useLocation } from "@docusaurus/router";
+
+const pathNameToPreviewText = (pathName: string) => {
+  const splitPaths = pathName.split("/").filter(x => x);
+  const probablyID = splitPaths[splitPaths.length - 1];
+  if (!probablyID) return "";
+  return probablyID
+    .split("-")
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
 
 export default function Layout(props) {
+  const { pathname } = useLocation();
+
+  const previewText = pathNameToPreviewText(pathname);  
+
   return (
     <>
       <Head>
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@arbitrum" />
-        <meta name="twitter:title" content="Arbitrum Docs!" />
+        <meta name="twitter:title" content={previewText || "Arbitrum Docs!"} />
         <meta
           name="twitter:description"
           content="Arbitrum Documentation Center"
