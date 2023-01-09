1b9942a40da8ac5e973d80a63d559cc35f19efb4
dzgoldman <dzgoldman@wesleyan.edu>
2023-01-09 17:44:41 -0500

    add AddressExplorerLink mdx componenet / clean up useful addresses

---
+++ website/src/components/AddressExplorerLink.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+type ChainID = 1 | 5 | 42170 | 421613 | 42161;
+
+const chainIDToExplorerUrlRool: {
+  [chainId in ChainID]: string;
+} = {
+  1: "https://etherscan.io/address",
+  5: "https://goerli.etherscan.io/address",
+  42161: "https://arbiscan.io/address",
+  42170: "https://nova.arbiscan.io/address",
+  421613: "https://goerli.arbiscan.io/address",
+};
+
+export const AddressExplorerLink = (props: {
+  address: string;
+  chainID: ChainID;
+}) => {
+  const { address, chainID } = props;
+  const rootUrl = chainIDToExplorerUrlRool[chainID];
+  // if(!rootUrl) throw new Error(`Error: no root url set for chain id ${chainID} `)
+  return (
+    <a href={`${rootUrl}/${address}`} target="_blank">
+      {address}
+    </a>
+  );
+};
