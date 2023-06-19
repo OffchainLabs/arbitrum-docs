758970ed9ea901408cc57aafc09a6fe09058fbd5
TucksonDev <tucksondev@gmail.com>
2023-06-19 18:18:24 +0100

    Rollback to simplest version

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,41 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import { useLocation, Redirect } from "@docusaurus/router";
-
+import { useLocation } from "@docusaurus/router";
 import styles from "./index.module.css";
 
-// NOTE: Path redirections have been moved to /website/src/resources/urlRedirections.js
-// and are being turned into a server-side configuration file in /website/src/scripts/generate_redirections.js.
-// Although this code is not going to be needed in production, we leave it for now as a fallback and
-// for testing redirections on local.
-
-const oldPathToNewPath = require("../resources/urlRedirections");
-
-const getNewPath = (_path) => {
-  const path = _path.toLowerCase();
-
-  for (let oldPath of Object.keys(oldPathToNewPath)) {
-    if (path.includes(oldPath)) {
-      return oldPathToNewPath[oldPath];
-    }
-  }
-};
+// NOTE: Path redirections have been moved to /vercel.json so they are handled in the 
+// server side by Vercel.
 
 function HomepageHeader(props) {
   const { siteConfig } = useDocusaurusContext();
   const location = useLocation();
-
-  if (props.notFound) {
-    let newPath = getNewPath(location.pathname);
-    if (newPath) {
-      console.info(`Redirecting from ${location.pathname} to ${newPath}`);
-      return <Redirect to={newPath} />;
-    }
-  }
 
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
