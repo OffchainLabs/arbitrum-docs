bd6298e1a3f3ba193a5ffb17ec60bfa164300e3f
Derek Lee <leeklderek@gmail.com>
2024-02-27 10:04:53 -0500

    docs: bump nitro version in globalvars.js to 2.3.0

--- website/src/resources/globalVars.js
+++ website/src/resources/globalVars.js
@@ -12,7 +12,7 @@ const sepoliaForceIncludePeriodBlocks = 5760;
 
 const globalVars = {
   // Node docker images
-  latestNitroNodeImage: 'offchainlabs/nitro-node:v2.2.5-a20a1c7',
+  latestNitroNodeImage: 'offchainlabs/nitro-node:v2.3.0-3e14543',
   latestClassicNodeImage: 'offchainlabs/arb-node:v1.4.5-e97c1a4',
 
   // Node snapshots (taken around April 20th, 2013)
@@ -29,7 +29,7 @@ const globalVars = {
 
   // Nitro Github references
   nitroRepositorySlug: 'nitro',
-  nitroVersionTag: 'v2.2.5',
+  nitroVersionTag: 'v2.3.0',
   nitroPathToPrecompiles: 'precompiles',
 
   nitroContractsRepositorySlug: 'nitro-contracts',
