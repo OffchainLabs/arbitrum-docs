f2ccd351ca710c9e124cbec7bf169067f45a34ac
leartulaj <leartulaj@gmail.com>
2024-03-04 14:42:20 -0500

    orbit infra providers
    options for 3rd part infrastructure services for deployers

---
+++ arbitrum-docs/launch-orbit-chain/infra-options-orbit-chains.md
@@ -0,0 +1,59 @@
+# Infrastructure Options for Orbit Chains
+
+A successful production-grade chain often requires additional surrounding infrastructure to enable app functionality and provide a suberb user experience for those interacting with the network. There are multiple options for various components for deployers to choose from, and the below is a non-comprehensive list of providers that Orbit teams can choose to work with if the need should arise.
+
+## Chain Operation
+
+Many application developers do not have the subject matter expertise required to deploy, operate, and maintain an independent production-grade network. In these cases, we highly recommend working with one of the following RaaS providers who will manage the necessary infrastructure and ensure a high degree of performance and security for your chain:
+
+- [Caldera](https://www.caldera.xyz/)
+- [Conduit](https://conduit.xyz/)
+- [AltLayer](https://altlayer.io/)
+- [Gelato](https://www.gelato.network/)
+
+## Bridges
+
+Orbit chains are easily launched with a canonical token bridge which permits transfers to and from their chain via Arbitrum One, Nova, or Ethereum. However, some applications may desire the ability to transfer to additional chains outside of the Orbit ecosystem or want to offer the ability to transfer across chain more quickly, without waiting for complete finality. In these instances, the following 3rd party bridging providers offer services to meet those needs:
+
+- [LayerZero](https://layerzero.network/)
+- [Connext](https://www.connext.network/)
+- [Hyperlane](https://www.hyperlane.xyz/)
+- [Axelar](https://axelar.network/)
+- [Across](https://across.to/)
+
+## Data Availability
+
+Deployers of Orbit chains may offer cheaper fees on their chain by storing the data posted in batches on an alternative Data Availability solution rather than to Ethereum directly. The built in functionality in Anytrust allows these developers to easily configure the cost of transacting and will enable more powerful features with additional chains that are also using Anytrust. However, there are integrations with Orbit that are available for those that would prefer to use one of the following third party services:
+
+- [Celestia](https://celestia.org/)
+- [EigenDA](https://www.eigenlayer.xyz/)
+- [Avail](https://www.availproject.org/) (coming soon!)
+- [Near](https://near.org/data-availability) (coming soon)
+
+## Indexers
+
+In order to surface necessary application information, teams frequently require additional indexing capabilities. Fetching application specific or historical information is difficult or impossible to do directly from an RPC. Fortunately, there are multiple data services providers that are familiar with operating this type of infrastructure, and may be willing to do so for your chain.
+
+- [Alchemy](https://www.alchemy.com/)
+- [The Graph](https://thegraph.com/)
+- [Goldsky](https://goldsky.com/)
+- [Ormi](https://www.ormilabs.xyz/)
+
+## Oracles
+
+Many applications require external data to be published on-chain in order to function properly. This data is usually either price feeds related to assets utilized in app or a source of verifiable randomness. The following oracle providers may be able to support your chain:
+
+- [Chainlink](https://chain.link/)
+- [Pyth](https://pyth.network/)
+- [Redstone](https://redstone.finance/)
+- [Randomizer](http://Randomizer.ai) (VRF only)
+- [Supra](https://supra.com/)
+
+## RPC
+
+RPCs are the primary interface for users and developers to interact with any chain, whether it be for transaction submission, reading state, or indexing historical data. There is often a need for robust RPC services for public consumption as well as for specific projects with higher rate limiting requirements. The following providers may support your Orbit chain:
+
+- [Alchemy](https://www.alchemy.com/)
+- [Ankr](https://www.ankr.com/)
+- [Chainstack](https://chainstack.com/)
+- [QuickNode](https://www.quicknode.com/)
--- website/sidebars.js
+++ website/sidebars.js
@@ -673,6 +673,429 @@ const sidebars = {
     },
     {
       type: 'category',
+      label: 'Build decentralized apps',
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'for-devs/quickstart-solidity-hardhat',
+          label: 'Quickstart (Solidity)',
+        },
+        {
+          type: 'doc',
+          label: 'Estimate gas',
+          id: 'devs-how-tos/how-to-estimate-gas',
+        },
+        {
+          type: 'doc',
+          label: 'Chains and testnets',
+          id: 'for-devs/concepts/public-chains',
+        },
+        {
+          type: 'doc',
+          label: 'Cross-chain messaging',
+          id: 'for-devs/cross-chain-messsaging',
+        },
+        {
+          type: 'category',
+          label: 'Arbitrum vs Ethereum',
+          items: [
+            {
+              type: 'doc',
+              label: 'Comparison overview',
+              id: 'for-devs/concepts/differences-between-arbitrum-ethereum/overview',
+            },
+            {
+              type: 'doc',
+              label: 'Block numbers and time',
+              id: 'for-devs/concepts/differences-between-arbitrum-ethereum/block-numbers-and-time',
+            },
+            {
+              type: 'doc',
+              label: 'RPC methods',
+              id: 'for-devs/concepts/differences-between-arbitrum-ethereum/rpc-methods',
+            },
+            {
+              type: 'doc',
+              label: 'Solidity support',
+              id: 'for-devs/concepts/differences-between-arbitrum-ethereum/solidity-support',
+            },
+          ],
+        },
+
+        {
+          type: 'category',
+          label: 'Oracles',
+          collapsed: true,
+          items: [
+            {
+              type: 'doc',
+              label: 'Overview',
+              id: 'for-devs/concepts/oracles',
+            },
+            {
+              type: 'doc',
+              label: 'Use oracles in your app',
+              id: 'devs-how-tos/how-to-use-oracles',
+            },
+            {
+              type: 'doc',
+              label: 'Reference',
+              id: 'for-devs/dev-tools-and-resources/oracles',
+            },
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Precompiles',
+          collapsed: true,
+          items: [
+            {
+              type: 'doc',
+              label: 'Overview',
+              id: 'for-devs/concepts/precompiles',
+            },
+            {
+              type: 'doc',
+              label: 'Reference',
+              id: 'for-devs/dev-tools-and-resources/precompiles',
+            },
+          ],
+        },
+        {
+          type: 'category',
+          label: 'NodeInterface',
+          collapsed: true,
+          items: [
+            {
+              type: 'doc',
+              label: 'Overview',
+              id: 'for-devs/concepts/nodeinterface',
+            },
+            {
+              type: 'doc',
+              label: 'Reference',
+              id: 'for-devs/dev-tools-and-resources/nodeinterface',
+            },
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Token bridging',
+          collapsed: true,
+          items: [
+            {
+              type: 'doc',
+              label: 'Overview',
+              id: 'for-devs/concepts/token-bridge/token-bridge-overview',
+            },
+            {
+              type: 'doc',
+              label: 'Get started',
+              id: 'devs-how-tos/bridge-tokens/how-to-bridge-tokens-overview',
+            },
+            {
+              type: 'doc',
+              label: 'Use the standard gateway',
+              id: 'devs-how-tos/bridge-tokens/how-to-bridge-tokens-standard',
+            },
+            {
+              type: 'doc',
+              label: 'Use the generic-custom gateway',
+              id: 'devs-how-tos/bridge-tokens/how-to-bridge-tokens-generic-custom',
+            },
+            {
+              type: 'doc',
+              label: 'Use the custom gateway',
+              id: 'devs-how-tos/bridge-tokens/how-to-bridge-tokens-custom-gateway',
+            },
+            {
+              type: 'doc',
+              label: 'Bridging Ether',
+              id: 'for-devs/concepts/token-bridge/token-bridge-ether',
+            },
+            {
+              type: 'doc',
+              label: 'Bridging ERC-20 tokens',
+              id: 'for-devs/concepts/token-bridge/token-bridge-erc20',
+            },
+          ],
+        },
+
+        {
+          type: 'category',
+          label: 'Reference',
+          items: [
+            {
+              type: 'doc',
+              id: 'node-running/node-providers',
+              label: 'RPC endpoints and providers',
+            },
+            {
+              type: 'doc',
+              label: 'Smart contract addresses',
+              id: 'for-devs/useful-addresses',
+            },
+            {
+              type: 'doc',
+              label: 'Chain parameters',
+              id: 'for-devs/chain-params',
+            },
+            {
+              type: 'doc',
+              label: 'Development frameworks',
+              id: 'for-devs/dev-tools-and-resources/development-frameworks',
+            },
+            {
+              type: 'doc',
+              label: 'Web3 libraries and tools',
+              id: 'for-devs/dev-tools-and-resources/web3-libraries-tools',
+            },
+            {
+              type: 'doc',
+              label: 'Monitoring tools and block explorers',
+              id: 'for-devs/dev-tools-and-resources/monitoring-tools-block-explorers',
+            },
+            {
+              type: 'doc',
+              label: 'Debugging tools',
+              id: 'for-devs/dev-tools-and-resources/debugging-tools',
+            },
+
+            {
+              type: 'doc',
+              id: 'mainnet-risks',
+              label: 'Mainnet risks',
+            },
+          ],
+        },
+        {
+          type: 'doc',
+          label: 'Troubleshooting',
+          id: 'for-devs/troubleshooting-building',
+        },
+        {
+          type: 'link',
+          label: 'Arbitrum SDK',
+          href: '/sdk',
+        },
+        {
+          type: 'link',
+          label: 'Tutorials',
+          href: 'https://github.com/OffchainLabs/arbitrum-tutorials',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Write Stylus contracts',
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'stylus/stylus-gentle-introduction',
+          label: 'A gentle introduction',
+        },
+        {
+          type: 'doc',
+          id: 'stylus/stylus-quickstart',
+          label: 'Quickstart (Rust)',
+        },
+        {
+          type: 'doc',
+          label: 'Run a local dev node',
+          id: 'stylus/how-tos/local-stylus-dev-node',
+        },
+        {
+          type: 'doc',
+          label: 'Testnets',
+          id: 'stylus/reference/testnet-information',
+        },
+        {
+          type: 'category',
+          label: 'Gas and ink',
+          collapsed: true,
+          items: [
+            {
+              type: 'doc',
+              label: 'Overview',
+              id: 'stylus/concepts/stylus-gas',
+            },
+            {
+              type: 'doc',
+              id: 'stylus/reference/opcode-hostio-pricing',
+              label: 'Gas and ink costs',
+            },
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Stylus SDK',
+          collapsed: true,
+          items: [
+            {
+              type: 'doc',
+              id: 'stylus/reference/rust-sdk-guide',
+              label: 'Rust SDK overview',
+            },
+            {
+              type: 'doc',
+              id: 'stylus/reference/stylus-sdk',
+              label: 'SDK repositories',
+            },
+          ],
+        },
+        {
+          type: 'doc',
+          label: 'Troubleshooting',
+          id: 'stylus/troubleshooting-building-stylus',
+        },
+
+        {
+          type: 'doc',
+          label: 'Add a new smart contract language',
+          id: 'stylus/how-tos/adding-support-for-new-languages',
+        },
+        {
+          type: 'doc',
+          label: 'Reduce the size of WASM binaries',
+          id: 'stylus/how-tos/optimizing-binaries',
+        },
+        {
+          type: 'link',
+          label: 'Rust crate docs',
+          href: 'https://docs.rs/stylus-sdk/latest/stylus_sdk/index.html',
+        },
+        {
+          type: 'link',
+          label: 'Source code repository',
+          href: 'https://github.com/OffchainLabs/stylus',
+        },
+        {
+          type: 'doc',
+          label: 'Public preview',
+          id: 'stylus/concepts/public-preview-expectations',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Run an Orbit chain',
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/orbit-gentle-introduction',
+          label: 'A gentle introduction',
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/orbit-quickstart',
+          label: 'Quickstart',
+        },
+        {
+          type: 'doc',
+          id: 'node-running/how-tos/running-an-orbit-node',
+          label: 'Run a full Orbit node',
+        },
+        {
+          type: 'category',
+          label: 'Customize your chain',
+          collapsed: true,
+          items: [
+            {
+              type: 'doc',
+              id: 'launch-orbit-chain/how-tos/customize-deployment-configuration',
+              label: `Customize deployment config`,
+            },
+            {
+              type: 'doc',
+              id: 'launch-orbit-chain/how-tos/customize-stf',
+              label: `Customize behavior`,
+            },
+            {
+              type: 'doc',
+              id: 'launch-orbit-chain/how-tos/customize-precompile',
+              label: `Customize precompiles`,
+            },
+          ],
+        },
+        {
+          type: 'category',
+          label: 'ArbOS',
+          collapsed: true,
+          items: [
+            {
+              type: 'html',
+              value:
+                '<a class="menu__link menu__list-item" href="/node-running/reference/arbos-software-releases/overview">ArbOS software releases</a>',
+              // q: why use an anchor html tag here?
+              // a: because this page lives in multiple sidebar sections, we pick one to be the "canonical" location for the page in the sidebar
+              //    if we link to them both via id or standard href, multiple sections of the sidebar will be opened at once when the user visits this page; we don't want that
+              //    if we use a fully qualified link, localhost won't work
+            },
+            {
+              type: 'doc',
+              id: 'launch-orbit-chain/how-tos/arbos-upgrade',
+              label: `Upgrade ArbOS`,
+            },
+          ],
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/how-tos/add-orbit-chain-to-bridge-ui',
+          label: `Add your chain to the bridge`,
+        },
+        {
+          type: 'html',
+          value:
+            '<a class="menu__link menu__list-item" href="/node-running/how-tos/data-availability-committee/introduction">Data Availability Committees</a>',
+          // q: why use an anchor html tag here?
+          // a: because this page lives in multiple sidebar sections, we pick one to be the "canonical" location for the page in the sidebar
+          //    if we link to them both via id or standard href, multiple sections of the sidebar will be opened at once when the user visits this page; we don't want that
+          //    if we use a fully qualified link, localhost won't work
+        },
+
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/concepts/anytrust-orbit-chain-keyset-generation',
+          label: 'Keyset generation (AnyTrust chains)',
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/concepts/chain-ownership',
+          label: 'Orbit chain ownership',
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/troubleshooting-building-orbit',
+          label: 'FAQ',
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/concepts/custom-gas-token-sdk',
+          label: 'Custom gas token SDK',
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/infra-options-orbit-chains',
+          label: '3rd Party Infrastructure Providers',
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/reference/command-line-options',
+          label: 'Command-line options',
+        },
+        {
+          type: 'doc',
+          id: 'launch-orbit-chain/concepts/public-preview-expectations',
+          label: 'Public preview',
+        },
+      ],
+    },
+
+    {
+      type: 'category',
       label: 'How Arbitrum works',
       collapsed: true,
       items: [
