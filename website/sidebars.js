c57ac024f1eadffa37f4065844f58cb95f217965
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2024-04-16 11:38:24 -0700

    fix: removed old prysm article from sidebars

--- website/sidebars.js
+++ website/sidebars.js
@@ -396,11 +396,6 @@ const sidebars = {
             },
             {
               type: 'doc',
-              id: 'launch-orbit-chain/how-tos/interactive-how-to/install-with-script',
-              label: `Orbit Chain Configuration`,
-            },
-            {
-              type: 'doc',
               id: 'launch-orbit-chain/how-tos/interactive-how-to/orbit-sdk-deploying-orbit-chain',
               label: `Orbit Chain Configuration`,
             },
