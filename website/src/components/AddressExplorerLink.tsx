6201037f9dcf65886e9411d879e3dafbe6bcbccd
TucksonDev <tucksondev@gmail.com>
2024-05-08 10:48:34 +0100

    Include v1 information

--- website/src/components/AddressExplorerLink.tsx
+++ website/src/components/AddressExplorerLink.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { getAddress, isAddress } from '@ethersproject/address';
 
-type ChainID = 1 | 11155111 | 42170 | 42161 | 421614 | 13331371;
+type ChainID = 1 | 11155111 | 42170 | 42161 | 421614 | 13331371 | 23011913;
 
 const chainIDToExplorerUrlRoot: {
   [chainId in ChainID]: string;
@@ -12,6 +12,7 @@ const chainIDToExplorerUrlRoot: {
   42170: 'https://nova.arbiscan.io/address',
   421614: 'https://sepolia.arbiscan.io/address',
   13331371: 'https://stylusv2-explorer.arbitrum.io/address',
+  23011913: 'https://stylus-testnet-explorer.arbitrum.io/address',
 };
 
 export const AddressExplorerLink = (props: {
