f4879279d8bed26fe4985c847e95a9f7eecd8957
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2024-06-03 07:39:35 -0700

    fix: temp exclusion of `src` for arbitrum-sdk typedoc generation

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -65,6 +65,74 @@ const config = {
       }),
     ],
   ],
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc',
+      {
+        id: 'arbitrum-sdk',
+        tsconfig: `${sdkCodebasePath}/tsconfig.json`,
+        entryPoints: [`${sdkCodebasePath}/src/lib`],
+        entryPointStrategy: 'expand',
+        exclude: [`abi`, `src`, `node_modules`, `tests`, `scripts`],
+        excludeNotDocumented: true,
+        excludeInternal: true,
+        excludeExternals: true,
+        readme: 'none',
+
+        // Output options
+        out: '../arbitrum-docs/sdk-docs',
+        hideGenerator: true,
+        validation: {
+          notExported: false,
+          invalidLink: true,
+          notDocumented: true,
+        },
+        logLevel: 'Verbose',
+        sidebar: {
+          autoConfiguration: false,
+        },
+
+        plugin: [
+          'typedoc-plugin-markdown',
+          `typedoc-plugin-frontmatter`,
+          './src/scripts/arbitrumSdkDocsHandler.ts',
+        ],
+
+        // typedoc-plugin-markdown options
+        // Reference: https://github.com/tgreyuk/typedoc-plugin-markdown/blob/next/packages/typedoc-plugin-markdown/docs/usage/options.md
+        outputFileStrategy: 'modules',
+        excludeGroups: false,
+        hidePageHeader: true,
+        hidePageTitle: true,
+        hideBreadcrumbs: true,
+        useCodeBlocks: true,
+        expandParameters: true,
+        parametersFormat: 'table',
+        propertiesFormat: 'table',
+        enumMembersFormat: 'table',
+        typeDeclarationFormat: 'table',
+        sanitizeComments: true,
+        frontmatterGlobals: {
+          layout: 'docs',
+          sidebar: true,
+          toc_max_heading_level: 5,
+        },
+      },
+    ],
+    [
+      'posthog-docusaurus',
+      {
+        apiKey: 'phc_AscFTQ876SsPAVMgxMmLn0EIpxdcRRq0XmJWnpG1SHL',
+        appUrl: 'https://app.posthog.com',
+        enableInDevelopment: false,
+        persistence: 'memory',
+        disable_session_recording: true,
+      },
+    ],
+    require.resolve('docusaurus-plugin-fathom'),
+    require.resolve('docusaurus-lunr-search'),
+    require.resolve('docusaurus-plugin-sass'),
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
