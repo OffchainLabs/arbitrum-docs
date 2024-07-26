1af5785d5d7f0f82e063803a6f7f8324c291d97c
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2024-07-26 16:15:54 -0700

    feat: add ImageCaption component

---
+++ website/src/components/ImageCaptions/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+export default function ImageWithCaption({ src, caption }) {
+  return (
+    <imageWithCaption style={{ padding: 20, display: 'block', align: 'center', color: 'gray' }}>
+      <img src={useBaseUrl(src)} alt={caption} />
+      <imgcaption>{`${caption}`}</imgcaption>
+    </imageWithCaption>
+  );
+}
