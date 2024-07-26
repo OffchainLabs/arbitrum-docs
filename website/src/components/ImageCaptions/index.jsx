5514736a02486d5130a84dc3af653de0a009bbde
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2024-07-26 16:39:46 -0700

    fix: remove unnecessary css propery

--- website/src/components/ImageCaptions/index.jsx
+++ website/src/components/ImageCaptions/index.jsx
@@ -3,7 +3,7 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function ImageWithCaption({ src, caption }) {
   return (
-    <imageWithCaption style={{ padding: 20, display: 'block', align: 'center', color: 'gray' }}>
+    <imageWithCaption style={{ padding: 20, display: 'block', color: 'gray' }}>
       <img src={useBaseUrl(src)} alt={caption} />
       <imgcaption>{`${caption}`}</imgcaption>
     </imageWithCaption>
