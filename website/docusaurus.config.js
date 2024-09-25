ebdf432be6e189e21f03b1bd3ae1fcc7606f3ed1
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2024-09-25 08:02:28 -0700

    feat: add with docusaurus' official mermaid integration

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -24,6 +24,9 @@ const config = {
   projectName: 'docusaurus', // Usually your repo name.
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
+  markdown: {
+    mermaid: true,
+  },
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
