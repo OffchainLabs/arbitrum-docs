5741165f865fc7295eadce733d2407425f0bfb95
Gaël Blanchemain <anegg0@users.noreply.github.com>
2024-10-29 09:55:59 -0700

    fix: changed component's fencing

--- website/src/components/Glossary/index.jsx
+++ website/src/components/Glossary/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { glossaryJsonData } from '@site/static/glossary.json';
 
 export default function Glossary({ glossaryJsonData }) {
   return (
     <div>
       {Object.entries(glossaryJsonData).map(([key, item]) => (
         <div key={key}>
-          <h3 id={key}>{item.title} {`{#${key}}`}</h3>
+          <h3 id={key}>{item.title} </h3>
           <div dangerouslySetInnerHTML={{ __html: item.text }} />
         </div>
       ))}
