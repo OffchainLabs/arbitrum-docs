27722ee3ef8b4e5addc15cc0514c95df85cda074
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2024-11-13 11:38:54 -0800

    fix: sidebar adjustments

--- website/sidebars.js
+++ website/sidebars.js
@@ -331,16 +331,6 @@ const sidebars = {
             },
             {
               type: 'doc',
-              id: 'launch-orbit-chain/how-tos/orbit-chain-finality',
-              label: `Configure delayed inbox finality`,
-            },
-            {
-              type: 'doc',
-              id: 'launch-orbit-chain/how-tos/manage-fee-collectors',
-              label: `Manage the fee collectors`,
-            },
-            {
-              type: 'doc',
               id: 'launch-orbit-chain/how-tos/customize-arbos',
               label: `Customize ArbOS version`,
             },
