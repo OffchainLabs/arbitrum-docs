37e86d51931d5e7f99b052bf3a59664ac74bad24
Doug Lance <4741454+douglance@users.noreply.github.com>
2025-01-27 13:08:34 -0500

    fix formatting

--- website/src/scripts/markdown-preprocessor.js
+++ website/src/scripts/markdown-preprocessor.js
@@ -1,12 +1,12 @@
 const globalVars = require('../resources/globalVars.js');
 
-function preprocessContent({filePath, fileContent}) {
+function preprocessContent({ filePath, fileContent }) {
   if (!filePath.endsWith('.mdx')) return fileContent;
 
   return fileContent.replace(
     /@@\s*([a-zA-Z0-9_-]+)=([^@]+)@@/g,
-    (match, varName, value) => globalVars[varName] || value.trim()
+    (match, varName, value) => globalVars[varName] || value.trim(),
   );
 }
 
-module.exports = preprocessContent; 
+module.exports = preprocessContent;
