a0dd534b10ba1b86840c677265c45f88ac46dbc3
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-02-06 10:25:59 -0800

    feat: replace basic <Circle/> with <Number4/> inside of <Modal/>

--- website/src/components/InteractiveDiagrams/Timeboost/CentralizedAuction/Modal.tsx
+++ website/src/components/InteractiveDiagrams/Timeboost/CentralizedAuction/Modal.tsx
@@ -3,7 +3,7 @@ import { useTransition, animated } from '@react-spring/web';
 import styled from 'styled-components';
 import * as Dialog from '@radix-ui/react-dialog';
 import { createPortal } from 'react-dom';
-import { Circle } from './Circle';
+import { Number4 } from './Number4';
 
 interface ModalProps {
   cx?: number;
@@ -48,7 +48,7 @@ export function Modal({
           zIndex: 10,
         }}
       >
-        <Circle cx={cx} cy={cy} r={13} fill="#FFFFFF" />
+        <Number4 cx={cx} cy={cy} />
       </g>
       {typeof document !== 'undefined' &&
         createPortal(
