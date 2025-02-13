ea858e3b37472ee985e7ae3538f8879e55053712
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-02-13 14:08:36 -0800

    Revert "Merge branch 'master' into diagrams-timeboost-gentle-intro"
    This reverts commit 274d795f384a9459aa8a12a251e85ca0d3c8e4d6, reversing
    changes made to 7f2372493bfc8d08c28611d6ee6b4b27badd8dec.

--- website/src/theme/MDXComponents/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MDXComponents from '@theme-original/MDXComponents';
-import ImageZoom from '@site/src/components/ImageZoom';
-
-export default {
-  ...MDXComponents,
-  ImageZoom,
-};
