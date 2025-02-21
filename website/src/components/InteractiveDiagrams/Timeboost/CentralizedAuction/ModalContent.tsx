ddf6340713bd23158727d20e4e6aac5883f04f32
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-02-21 13:58:26 -0800

    feat: remove unneeded components

--- website/src/components/InteractiveDiagrams/Timeboost/CentralizedAuction/ModalContent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const modalContent = () => {
-  return (
-    <div>
-      <p>This is a placeholder for the modal content. Each step will describe the centralized auction process.</p>
-    </div>
-  );
-};
