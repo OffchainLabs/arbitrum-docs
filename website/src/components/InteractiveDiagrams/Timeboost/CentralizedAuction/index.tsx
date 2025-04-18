36a0bab70e20744046ce774c3c522738a9703cc6
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-04-18 01:26:28 -0700

    refactoring: converted index.jsx to TS

--- website/src/components/InteractiveDiagrams/Timeboost/CentralizedAuction/index.tsx
+++ website/src/components/InteractiveDiagrams/Timeboost/CentralizedAuction/index.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { Modal } from './Modal';
 import { NumberComponent } from './NumberComponent';
 
-export default function CentralizedAuction() {
+export default function CentralizedAuction(): JSX.Element {
   return <FlowChart />;
 }
 
-export const FlowChart = (props) => (
+export const FlowChart: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     id="svg415"
@@ -2311,22 +2311,12 @@ export const FlowChart = (props) => (
           d="M256.03 618.93v-63.6c0-6.61 5.36-11.97 11.97-11.97h138.61c6.61 0 11.97-5.36 11.97-11.97V294.43c0-6.61 5.36-11.97 11.97-11.97h54.36"
           className="cls-4"
         />
-        <NumberComponent
-          number={1}
-        />
-        <Modal
-          number={2}
-        />
-        <Modal
-          number={3}
-        />
-        <Modal
-          number={4}
-        />
-        <NumberComponent
-          number={5}
-        />
-        </g>
+        <NumberComponent number={1} />
+        <Modal number={2} />
+        <Modal number={3} />
+        <Modal number={4} />
+        <NumberComponent number={5} />
+      </g>
     </g>
   </svg>
 );
