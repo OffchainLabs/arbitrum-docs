ad932b0c73bdf44e07d170813e65282ec9c428e0
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-04-19 08:48:09 -0700

    refactor: fix attempt on Circle.animated method call

--- website/src/components/InteractiveDiagrams/Timeboost/PictureWithClickableNumbers/NumberComponent.tsx
+++ website/src/components/InteractiveDiagrams/Timeboost/PictureWithClickableNumbers/NumberComponent.tsx
@@ -86,7 +86,6 @@ export const NumberComponent: React.FC<
 
   return (
     <g id={`number${number}`}>
-      {/* Add interactive buttons based on shouldBeInteractive */}
       {shouldBeInteractive && (
         <ButtonComponent
           x={coords.circle.x - CIRCLE_RADIUS}
@@ -95,9 +94,8 @@ export const NumberComponent: React.FC<
           height={CIRCLE_RADIUS * 2}
         />
       )}
-      {/* The circle background for the number */}
       {shouldAnimate ? (
-        <animated.circle
+        <animated
           id={`circle${number}`}
           cx={coords.circle.x}
           cy={coords.circle.y}
@@ -115,7 +113,6 @@ export const NumberComponent: React.FC<
           style={{ opacity: 1 }}
         />
       )}
-      {/* The number path (SVG shape of the number) */}
       <path
         id={`path${number}`}
         d={pathData}
