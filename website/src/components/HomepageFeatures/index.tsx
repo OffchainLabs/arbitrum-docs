3f4bda2ff2aa835e35352a089a3380446297f0ca
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-05-27 15:51:54 -0700

    fix: repair broken links

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -27,7 +27,7 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Quickstart: Run a node',
     Svg: require('@site/static/img/node.svg').default,
-    href: '/run-arbitrum-node/node-types',
+    href: '/run-arbitrum-node/overview',
     description: 'Learn how to run a node to interact with any Arbitrum network.',
   },
   {
