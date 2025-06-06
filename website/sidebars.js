de473486a7e05ddfd75d3440a91984b476442341
Pete <petevielhaber@gmail.com>
2025-06-06 15:34:29 -0500

    moving calc aep fees to af

--- website/sidebars.js
+++ website/sidebars.js
@@ -349,12 +349,7 @@ const sidebars = {
                       type: 'doc',
                       id: 'launch-arbitrum-chain/configure-your-chain/advanced-configurations/aep-fee-router/set-up-aep-fee-router',
                       label: `Set up AEP fee router`,
-                    },
-                    {
-                      type: 'doc',
-                      id: 'launch-arbitrum-chain/configure-your-chain/advanced-configurations/aep-fee-router/calculate-aep-fees',
-                      label: `Calculate AEP license fees`,
-                    },
+                    }
                   ],
                 },
               ],
