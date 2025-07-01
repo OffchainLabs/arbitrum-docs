d9ff36fb92448e686ac8840f4acb785d6e1c2c14
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-07-01 14:44:57 -0700

    place deploy timeboost article in sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -342,6 +342,11 @@ const sidebars = {
                 },
                 {
                   type: 'doc',
+                  id: 'launch-arbitrum-chain/configure-your-chain/advanced-configurations/deploy-timeboost',
+                  label: 'Deploy and configure Timeboost',
+                },
+                {
+                  type: 'doc',
                   label: 'Use Layer Leap',
                   id: 'launch-arbitrum-chain/configure-your-chain/advanced-configurations/layer-leap',
                 },
