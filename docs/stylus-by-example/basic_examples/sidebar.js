6a133e7eb550b83fe216c251779bb36c631d5309
Pete <petevielhaber@gmail.com>
2025-08-01 11:22:17 -0500

    adding images for inside arbitrum nitro; and updating image links

---
+++ docs/stylus-by-example/basic_examples/sidebar.js
@@ -0,0 +1,82 @@
+// @ts-check
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebar = {
+  "items": [
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/hello_world",
+      "label": "Hello World"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/primitive_data_types",
+      "label": "Primitive Data Types"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/variables",
+      "label": "Variables"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/constants",
+      "label": "Constants"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/function",
+      "label": "Function"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/errors",
+      "label": "Errors"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/events",
+      "label": "Events"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/inheritance",
+      "label": "Inheritance"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/vm_affordances",
+      "label": "Vm Affordances"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/sending_ether",
+      "label": "Sending Ether"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/function_selector",
+      "label": "Function Selector"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/abi_encode",
+      "label": "Abi Encode"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/abi_decode",
+      "label": "Abi Decode"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/hashing",
+      "label": "Hashing"
+    },
+    {
+      "type": "doc",
+      "id": "stylus-by-example/basic_examples/bytes_in_bytes_out",
+      "label": "Bytes In Bytes Out"
+    }
+  ]
+};
+module.exports = sidebar.items;
