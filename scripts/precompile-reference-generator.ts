895fb0f8b067b6d87b786540433e963c42cc866f
Harry Kalodner <412311+hkalodner@users.noreply.github.com>
2025-08-12 09:02:56 -0400

    Fix precompile generation and add error checking

--- scripts/precompile-reference-generator.ts
+++ scripts/precompile-reference-generator.ts
@@ -252,12 +252,19 @@ const generatePrecompileReferenceTables = async (
       .replace('github.com', 'raw.githubusercontent.com')
       .replace('blob/', '')}${precompileName}.sol`,
   );
+  if (!interfaceCodeRawResponse.ok) {
+    throw new Error(`Failed fetching precompile ${precompileName} interface with status ${interfaceCodeRawResponse.status}`);
+  }
+  
   const interfaceCode = await interfaceCodeRawResponse.text();
   const implementationCodeRawResponse = await fetch(
     `${implementationBaseUrl
       .replace('github.com', 'raw.githubusercontent.com')
       .replace('blob/', '')}${precompileName}.go`,
   );
+  if (!implementationCodeRawResponse.ok) {
+    throw new Error(`Failed fetching precompile ${precompileName} implementation with status ${implementationCodeRawResponse.status}`);
+  }
   const implementationCode = await implementationCodeRawResponse.text();
 
   const methodsTable = renderMethodsInTable(
