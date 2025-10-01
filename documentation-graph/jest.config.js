b103d3136b8fb027f605393e4a920fea3146940a
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-09-30 19:17:08 -0700

    test: add Jest configuration for testing infrastructure

--- documentation-graph/jest.config.js
+++ documentation-graph/jest.config.js
@@ -1,28 +1,19 @@
 export default {
   testEnvironment: 'node',
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  testMatch: [
-    '**/test/**/*.test.js',
-    '**/?(*.)+(spec|test).js'
-  ],
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/index.js',
-    '!**/node_modules/**',
-    '!**/dist/**'
-  ],
-  coverageThresholds: {
+  testMatch: ['**/test/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: ['src/**/*.js', '!src/index.js', '!**/node_modules/**', '!**/dist/**'],
+  coverageThreshold: {
     global: {
       branches: 50,
       functions: 50,
       lines: 50,
-      statements: 50
-    }
+      statements: 50,
+    },
   },
   setupFilesAfterEnv: ['jest-extended/all'],
-  verbose: true
+  verbose: true,
 };
