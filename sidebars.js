a86d75fc375d42f3b0b767b2b5d9639c22021459
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2026-03-26 16:29:48 -0700

    Revert sidebar change (sidebar entries added in separate PR)

--- sidebars.js
+++ sidebars.js
@@ -955,11 +955,6 @@ const sidebars = {
         },
         {
           type: 'doc',
-          id: 'run-arbitrum-node/nitro/docker-and-cli-binaries',
-          label: 'Docker and CLI binaries',
-        },
-        {
-          type: 'doc',
           id: 'run-arbitrum-node/nitro/build-nitro-locally',
           label: 'Build Nitro locally',
         },
