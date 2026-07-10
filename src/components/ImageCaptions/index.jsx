3133de5391a1e3b46c6cd8a6e19c059c0dbe0146
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2026-07-10 12:12:13 -0700

    add dedicated css styling to <imagewithcaption> component

--- src/components/ImageCaptions/index.jsx
+++ src/components/ImageCaptions/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.scss';
 
 export default function ImageWithCaption({ src, caption }) {
   return (
-    <imageWithCaption style={{ padding: 20, display: 'block', color: 'gray', textAlign: 'center' }}>
-      <img src={useBaseUrl(src)} alt={caption} style={{ borderRadius: '0.4rem' }} />
-      <imgcaption style={{ display: 'block', marginTop: 8 }}>{`${caption}`}</imgcaption>
+    <imageWithCaption className={styles.figure}>
+      <img src={useBaseUrl(src)} alt={caption} className={styles.image} />
+      <imgcaption className={styles.caption}>{`${caption}`}</imgcaption>
     </imageWithCaption>
   );
 }
