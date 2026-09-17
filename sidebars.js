391397257fe36a1a34de4c8b22df48b203559eea
Pete <petevielhaber@gmail.com>
2026-09-17 12:09:03 -0500

    restoring pga

--- sidebars.js
+++ sidebars.js
@@ -379,6 +379,11 @@ const sidebars = {
                   id: 'launch-arbitrum-chain/chain-config/sequencer/timeboost',
                   label: `Timeboost`,
                 },
+                {
+                  type: 'doc',
+                  id: 'launch-arbitrum-chain/chain-config/sequencer/pga',
+                  label: `PGA`,
+                },
               ],
             },
             {
@@ -1135,6 +1140,22 @@ const sidebars = {
               type: 'link',
               href: 'https://arxiv.org/abs/2306.02179',
               label: 'White paper: Timeboost',
+            },
+          ],
+        },
+        {
+          type: 'category',
+          label: 'PGA and Fast Feed',
+          items: [
+            {
+              type: 'doc',
+              id: 'how-arbitrum-works/priority-gas-auction/pga',
+              label: 'How PGA works',
+            },
+            {
+              type: 'doc',
+              id: 'how-arbitrum-works/priority-gas-auction/fast-feed',
+              label: 'Introduction to the Fast Feed',
             },
           ],
         },
